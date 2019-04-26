2ce2d42ae6a4f56dd1e809b5644e3854ed14fda6
Ethan Sagin <ethansagin@gmail.com>
2019-04-26 15:35:59 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,17 @@
+class Author 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts
+    @posts
+  end
+  
+  def add_post(a_post)
+    new_post = Post.new(a_post)
+  
+  
+end
